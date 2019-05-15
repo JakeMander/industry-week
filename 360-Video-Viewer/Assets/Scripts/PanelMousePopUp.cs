a61b7d88ee01df2de30db89d20036bd2803670a6
Jake Mander <jmmander1@hotmail.co.uk>
2019-05-15 19:51:32 +0100

    Added Quest Markers
    Added Quest Markers To Assets

---
+++ 360-Video-Viewer/Assets/Scripts/PanelMousePopUp.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PanelMousePopUp : MonoBehaviour
+{
+
+    public GameObject Panel;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Panel.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(2))
+        {
+            Panel.gameObject.SetActive(true);
+        }
+    }
+}
