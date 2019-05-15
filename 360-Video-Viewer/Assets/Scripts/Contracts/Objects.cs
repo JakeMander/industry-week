152abdee8ee95050149d66045673bcabbaec9617
David Morgan <Thomasmorgan138@Outlook.com>
2019-05-15 16:11:19 +0100

    *WIP*

--- 360-Video-Viewer/Assets/Scripts/Contracts/Objects.cs
+++ 360-Video-Viewer/Assets/Scripts/Contracts/Objects.cs
@@ -7,4 +7,9 @@ public class Objects
     public int id;
     public string type;
     public int position;
+    public string annotation;
+    public string title;
+    public string time;
+    public string severity;
+    public string author;
 }
